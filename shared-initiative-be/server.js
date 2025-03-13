8768f47827cb9ed5041a710fad6ed9517387b250
Alessandro Crispini <alessandro.crispini@rgigroup.com>
2025-03-13 12:38:20 +0100

    feat: first commit

---
+++ shared-initiative-be/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../shared-initiative-fe/dist/shared-initiative-fe')));
+
+app.get('/initiative', (req, res) => {
+    res.sendFile(path.join(__dirname, '../shared-initiative-fe/dist/shared-initiative-fe/browser/index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World from Node.js server!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
